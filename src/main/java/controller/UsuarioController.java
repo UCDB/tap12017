358aadece003c4b3a04b5128cc6b96947fd2b718
virmerson <virmerson@hightechcursos.com.br>
2017-03-06 22:07:35 -0400

    Gerando Json e Salvando em list

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,18 +10,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/usucontroller")
-public class UsuarioController extends  HttpServlet {
+import model.Usuario;
+
+@WebServlet(urlPatterns = "/usucontroller")
+public class UsuarioController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	private List<Usuario> lista = new ArrayList<>();
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//Capturando o que vem do client
+		// Capturando o que vem do client
+		String nome = req.getParameter("nome");
 		String email = req.getParameter("email");
-		//Respondendo ao client
-		resp.getWriter().print("Olá "  + email  );
+
+		// Instanciando objeto
+		Usuario usu = new Usuario(nome, email);
+
+		// Inserir na lista
+		lista.add(usu);
+
+		resp.getWriter().println("{ nome:" + usu.getNome() + "  , email: " + usu.getEmail() + "  } ");
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String json = "[";
+		for (int i = 0; i < lista.size(); i++) {
+
+			json += "{ nome:" + lista.get(i).getNome() + "  , email: " + lista.get(i).getEmail() + "  }";
+			if (i < lista.size()-1)
+				json += ",";
+		}
+		json += "]";
+
+		resp.getWriter().print(json);
+
 	}
 
 }
